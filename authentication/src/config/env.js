dc85516ad2461fe139d18744335e4d13bffa522b
duccluongg <duccluongg99@gmail.com>
2022-11-02 22:15:56 +0700

    updatte

--- authentication/src/config/env.js
+++
@@ -1,7 +0,0 @@
-const dotenv =  require('dotenv');
-
-dotenv.config();
-
-exports.MONGO_URL = process.env.MONGO_URL;
-
-exports.SECRET_KEY = process.env.SECRET_KEY;
--- gateway/index.js
+++ gateway/index.js
@@ -7,9 +7,9 @@ const app = express()
 app.use(cors());
 app.use(express.json())
 
-app.use('/authentication', proxy('http://localhost:8001'))
-app.use('/product', proxy('http://localhost:8002'))
-app.use('/shopping', proxy('http://localhost:8003'))
+app.use('/billing', proxy('http://localhost:8004'))
+app.use('/order', proxy('http://localhost:8003'))
+
 
 app.listen(8000, () => {
     console.log('ApiGateway is Listening to Port 8000')
